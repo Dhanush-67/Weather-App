816eee342a256e27a049be5f6d921b6b14a5ed66
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2026-01-22 13:51:08 -0500

    Added project files

---
+++ weather-app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-950 text-white">
+      <h1 className="text-4xl font-bold">Tailwind is working ✅</h1>
+    </div>
+  );
+}
+
+export default App;
