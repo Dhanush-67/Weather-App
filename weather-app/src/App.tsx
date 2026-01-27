a25e979cd3107402951a317ac0d6fe805a10d9fb
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2026-01-27 01:29:18 -0500

    Setup the homepage with darkmode enabled

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import { Button } from "@/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-zinc-950 text-white">
-      <h1 className="text-4xl font-bold">Tailwind is working ✅</h1>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
