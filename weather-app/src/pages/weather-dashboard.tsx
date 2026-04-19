3870bc5207d4cec5d49e251e1069c37797fe016e
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2026-04-19 15:07:04 -0400

    Added location-error popup ui

--- weather-app/src/pages/weather-dashboard.tsx
+++ weather-app/src/pages/weather-dashboard.tsx
@@ -1,8 +1,10 @@
-import React from "react";
 import { Button } from "@/components/ui/button";
-import { RefreshCw } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle, RefreshCw } from "lucide-react";
+import { MapPin } from "lucide-react";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { useEffect } from "react";
+import WeatherSkeleton from "@/components/loading-skeleton";
 
 const WeatherDashboard = () => {
   const {
@@ -21,6 +23,23 @@ const WeatherDashboard = () => {
   };
 
   if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive" className="max-w-md">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
   }
 
   return (
@@ -31,8 +50,8 @@ const WeatherDashboard = () => {
         <Button
           variant={"outline"}
           size={"icon"}
-          /* onClick={handleRefresh}
-          disabled = {} */
+          onClick={handleRefresh}
+          // disabled = {}
         >
           <RefreshCw className="h-4 w-4" />
         </Button>
